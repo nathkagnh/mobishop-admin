11c6c094782e5275e233f23564199ca76566c993
nathkagnh <nathkagnh@gmail.com>
2015-10-29 17:46:25 +0700

    xxx

--- module/Admin/src/Admin/Controller/ManufactureController.php
+++ module/Admin/src/Admin/Controller/ManufactureController.php
@@ -6,8 +6,20 @@ use Zend\View\Model\ViewModel;
 
 class ManufactureController extends AbstractActionController
 {
+	protected $manufactureTable;
+
 	public function indexAction()
 	{
 		return new ViewModel();
+	}
+
+	public function getManufactureTable()
+	{
+		if(!$manufactureTable)
+		{
+			$sm=$this->getServiceLocator();
+			$this->manufactureTable=$sm->get('Admin\Model\ManufactureTable');
+		}
+		return $this->manufactureTable;
 	}
 }
