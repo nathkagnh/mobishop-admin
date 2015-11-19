4fa4d623a2c955658abe1b6a31f91917caad6f8e
nathkagnh <nathkagnh@gmail.com>
2015-11-19 08:59:20 +0700

    update database

--- mobishop.sql
+++ mobishop.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2015 at 04:55 AM
--- Server version: 5.6.17
+-- Generation Time: Nov 19, 2015 at 02:58 AM
+-- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,6 +19,73 @@ SET time_zone = "+00:00";
 --
 -- Database: `mobishop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advertise`
+--
+
+CREATE TABLE IF NOT EXISTS `advertise` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `link` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `date_start` date NOT NULL,
+  `date_end` date NOT NULL,
+  `view_count` int(11) NOT NULL,
+  `click_count` int(11) NOT NULL,
+  `show` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `advertise`
+--
+
+INSERT INTO `advertise` (`id`, `location`, `image`, `link`, `date_start`, `date_end`, `view_count`, `click_count`, `show`) VALUES
+(1, 'top', 'lion-photo_564bf1fa6562e.jpg', 'http://nhatkhang.admin:81/advertise/create', '2015-11-18', '2015-11-19', 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manufacture`
+--
+
+CREATE TABLE IF NOT EXISTS `manufacture` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `tel` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `manufacture`
+--
+
+INSERT INTO `manufacture` (`id`, `name`, `address`, `tel`) VALUES
+(1, 'Asus', 'dsadsa', 3242342);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `price` float NOT NULL,
+  `manufacture` int(11) NOT NULL,
+  `image` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `detail` varchar(2000) COLLATE utf8_unicode_ci NOT NULL,
+  `inventory_number` int(11) NOT NULL,
+  `sale_number` int(11) NOT NULL,
+  `show` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -60,14 +127,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `status` int(11) NOT NULL DEFAULT '0',
   `admin` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `email`, `pass`, `name`, `address`, `tel`, `status`, `admin`) VALUES
-(1, 'nathkagnh@gmail.com', 'dsdadd', 'Lê Đỗ Nhật Khang', 'Cách Mạng Tháng 8', 238943294, 1, 1),
+(1, 'nathkagnh@gmail.com', '1234', 'Lê Đỗ Nhật Khang', 'Cách Mạng Tháng 8', 238943294, 1, 1),
 (5, 'nathkagnh@gmail.com.vn', 'fasf', 'Nhật Khang', 'asfafa', 2142342, 1, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
