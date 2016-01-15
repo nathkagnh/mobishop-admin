d6a1f556189758f66afeaed89b7dbc42476bd346
nathkagnh <nathkagnh@gmail.com>
2016-01-15 16:09:59 +0700

    update create product

--- module/Admin/src/Admin/Controller/ProductController.php
+++ module/Admin/src/Admin/Controller/ProductController.php
@@ -9,18 +9,22 @@ use Admin\Model\Product;
 class ProductController extends AbstractActionController
 {
 	protected $productTable;
+	protected $manufactureTable;
 
 	public function indexAction()
 	{
 		$products=$this->getProductTable()->fetchAll();
 		return new ViewModel(array(
-			'products'=>$products,
+			'products'=>$products
 			));
 	}
 
 	public function createAction()
 	{
-		return new ViewModel();
+		$manufactures=$this->getManufactureTable()->fetchAll();
+		return new ViewModel(array(
+			'manufactures'=>$manufactures
+			));
 	}
 
 	public function postAction()
@@ -41,5 +45,15 @@ class ProductController extends AbstractActionController
 			$this->productTable=$sm->get('Admin\Model\ProductTable');
 		}
 		return $this->productTable;
+	}
+
+	public function getManufactureTable()
+	{
+		if(!$this->manufactureTable)
+		{
+			$sm=$this->getServiceLocator();
+			$this->manufactureTable=$sm->get('Admin\Model\ManufactureTable');
+		}
+		return $this->manufactureTable;
 	}
 }
--- public/js/create.js
+++ public/js/create.js
@@ -27,47 +27,64 @@ function showStep3(){
 // Show step 1 (default)
 showStep1();
 
+// This is script of step 2
+$(document).on('change', 'input[name="img_upload[]"]', function(e){
+	var current = curr = $('#files').data('current');
+
+	var reader = new FileReader();
+	reader.onload = function(event){
+		$('#thumbs').append('<div class="block-image">'
+			+'<img src="'+event.target.result+'" class="thumbs">'
+			+'<span class="delete-image" data-id="'+curr+'"></span>'
+			+'</div>'
+			);
+	};
+	reader.readAsDataURL(e.target.files[0]);
+
+	$(this).css({display: 'none'});
+	current++;
+	$('#files').append('<input type="file" id="image'+current+'" data-id="'+current+'" name="img_upload[]" accept="image/*" style="display: inline;">');
+	$('#files').data('current', current);
+});
+
+$(document).on('click', '.delete-image', function(e){
+	var id = $(this).data('id');
+	$(this).parent().fadeOut(150, function(){
+		$(this).remove();
+	});
+	$('#image'+id).remove();
+});
+
 // This is script of step 3
-var total_fields = 0;
+var current_field = total_field = 0;
+var arrFieldId = [];
 $('#add_field').click(function(){
-	if(total_fields==0){
-		$('#table_step3').append('<tr><td><span id="field_remove" data-id="'+total_fields+'" title="remove">&Chi;</span><input type="text" class="text_field_name" id="name'+total_fields+'" name="field_name'+total_fields+'" placeholder="typing field name...">: </td><td><input type="text" class="text" id="field'+total_fields+'" name="field_content'+total_fields+'" placeholder="typing content..."></td></tr>');
-		total_fields++;
+	if(current_field==0){
+		$('#table_step3').append('<tr><td><span id="field_remove" data-id="'+current_field+'" title="remove"></span><input type="text" class="text_field_name" id="name'+current_field+'" name="field_name'+current_field+'" placeholder="typing field name...">: </td><td><input type="text" class="text" id="field'+current_field+'" name="field_content'+current_field+'" placeholder="typing content..."></td></tr>');
+		arrFieldId.push(0);
+		current_field++;
 	}
-	else if(total_fields>0){
-		if($("input[name='field_name"+(total_fields-1)+"']").val()=='' && $("input[name='field_name"+(total_fields-1)+"']").parent().parent().css('display')!='none')
-			$("input[name='field_name"+(total_fields-1)+"']").focus();
-		else if($("input[name='field_content"+(total_fields-1)+"']").val()=='' && $("input[name='field_content"+(total_fields-1)+"']").parent().parent().css('display')!='none')
-			$("input[name='field_content"+(total_fields-1)+"']").focus();
+	else if(current_field>0){
+		if($("input[name='field_name"+(current_field-1)+"']").val()=='' && $("input[name='field_name"+(current_field-1)+"']").parent().parent().css('display')!='none')
+			$("input[name='field_name"+(current_field-1)+"']").focus();
+		else if($("input[name='field_content"+(current_field-1)+"']").val()=='' && $("input[name='field_content"+(current_field-1)+"']").parent().parent().css('display')!='none')
+			$("input[name='field_content"+(current_field-1)+"']").focus();
 		else{
-			$('#table_step3').append('<tr><td><span id="field_remove" data-id="'+total_fields+'" title="remove">&Chi;</span><input type="text" class="text_field_name" id="name'+total_fields+'" name="field_name'+total_fields+'" placeholder="typing field name...">: </td><td><input type="text" class="text" id="field'+total_fields+'" name="field_content'+total_fields+'" placeholder="typing content..."></td></tr>');
-			total_fields++;
+			$('#table_step3').append('<tr><td><span id="field_remove" data-id="'+current_field+'" title="remove"></span><input type="text" class="text_field_name" id="name'+current_field+'" name="field_name'+current_field+'" placeholder="typing field name...">: </td><td><input type="text" class="text" id="field'+current_field+'" name="field_content'+current_field+'" placeholder="typing content..."></td></tr>');
+			arrFieldId.push(current_field);
+			current_field++;
 		}
 	}
 });
 $(document).on('click', '#field_remove', function(){
 	var id=$(this).data('id');
-	$("input[name='field_name"+id+"']").val('');
-	$(this).parent().parent().fadeOut(150);
+	$(this).parent().parent().fadeOut(150, function(){
+		$(this).remove();
+		arrFieldId.pop(id);
+	});
 });
-$('#add_field').click(function(){
-	if(total_fields==0){
-		$('#table_step3').append('<tr><td><span id="field_remove" data-id="'+total_fields+'" title="remove">&Chi;</span><input type="text" class="text_field_name" id="name'+total_fields+'" name="field_name'+total_fields+'" placeholder="typing field name...">: </td><td><input type="text" class="text" id="field'+total_fields+'" name="field_content'+total_fields+'" placeholder="typing content..."></td></tr>');
-		total_fields++;
-	}
-	else if(total_fields>0){
-		if($("input[name='field_name"+(total_fields-1)+"']").val()=='' && $("input[name='field_name"+(total_fields-1)+"']").parent().parent().css('display')!='none')
-			$("input[name='field_name"+(total_fields-1)+"']").focus();
-		else if($("input[name='field_content"+(total_fields-1)+"']").val()=='' && $("input[name='field_content"+(total_fields-1)+"']").parent().parent().css('display')!='none')
-			$("input[name='field_content"+(total_fields-1)+"']").focus();
-		else{
-			$('#table_step3').append('<tr><td><span id="field_remove" data-id="'+total_fields+'" title="remove">&Chi;</span><input type="text" class="text_field_name" id="name'+total_fields+'" name="field_name'+total_fields+'" placeholder="typing field name...">: </td><td><input type="text" class="text" id="field'+total_fields+'" name="field_content'+total_fields+'" placeholder="typing content..."></td></tr>');
-			total_fields++;
-		}
-	}
-});
-$(document).on('click', '#field_remove', function(){
-	var id=$(this).data('id');
-	$("input[name='field_name"+id+"']").val('');
-	$(this).parent().parent().fadeOut(150);
+
+// Create product
+$('#btn-create').click(function(e){
+	
 });
